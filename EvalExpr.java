6c31c2f638db50ae1516d3b0f790f465bf235490
Sukhmani Choudhry <55005543+Schoud1@users.noreply.github.com>
2019-09-07 17:31:35 -0400

    Update EvalExpr.java

--- EvalExpr.java
+++ EvalExpr.java
@@ -1,11 +1,3 @@
-
-/*
-THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING     
-CODE WRITTEN BY OTHER STUDENTS. _Sukhmani_Choudhry_
-*/
-
-
-
 public class EvalExpr
 {
    /* ===========================================================
